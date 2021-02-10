7a225dc4a520425504c895d65ce25c188d2b71b4
seokjin lim <seokjinlim@seokjinui-MacBookAir.local>
2021-02-10 22:39:42 +0900

    ETF,KOSPI,KOSDAQ ticker list 가지고 오기

--- stock_base.py
+++ stock_base.py
@@ -2,7 +2,8 @@ import pandas as pd
 from pykrx import stock
 from datetime import date
 
-
+#날짜 포맷 YYYYMMDD
+to_day = date.today().isoformat().replace('-','')
 
 # 1. 수집
 # 1.1 주식 종목을 가지고 온다.
@@ -11,10 +12,18 @@ from datetime import date
 # df.to_csv('./stock_list/stock_list_'+date.today().isoformat()+'.scv')
 
 # tickers = stock.get_market_ticker_list("20200209", market='ALL')
-tickers = stock.get_etf_ticker_list("20200209")
-tickers = pd.DataFrame(tickers)
-tickers.to_csv('./stock_list/stock_etf_list_'+date.today().isoformat()+'.scv')
+tickers = pd.DataFrame(stock.get_etf_ticker_list(to_day))
+tickers.to_csv('./stock_list/ETF_ticker_list_'+date.today().isoformat()+'.scv')
+tickers = pd.DataFrame(stock.get_market_ticker_list(to_day, market='KOSPI'))
+tickers.to_csv('./stock_list/KOSPI_ticker_list_'+date.today().isoformat()+'.scv')
+tickers = pd.DataFrame(stock.get_market_ticker_list(to_day, market='KOSDAQ'))
+tickers.to_csv('./stock_list/KOSDAQ_ticker_list_'+date.today().isoformat()+'.scv')
 
+
+tickers ={}
+tickers['kospi_tickers'] = ''
+tickers['kosdaq_tickers'] =''
+tickers['etf_tickers'] = ''
 
 
 
