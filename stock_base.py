d157b88cf134540b8532b2839efe3b0ac3b1d1cf
seokjin window PC <hopesukjin@gmail.com>
2021-02-15 18:02:21 +0900

    수정

--- stock_base.py
+++ stock_base.py
@@ -2,23 +2,13 @@ import pandas as pd
 from pykrx import stock
 from datetime import date
 
-<<<<<<< HEAD
-=======
 #날짜 포맷 YYYYMMDD
->>>>>>> 3d49ecd2351e6949ff54a73f183f81cfc84a1c02
 to_day = date.today().isoformat().replace('-','')
 
 # 1. 수집
 # 1.1 주식 종목을 가지고 온다.
  # 1.1.1 신규 종목 가지고 오기. - ETF,KOSPI,KOSDAQ
 
-# tickers = stock.get_market_ticker_list("20200209", market='ALL')
-<<<<<<< HEAD
-tickers = stock.get_etf_ticker_list(to_day)
-tickers = pd.DataFrame(tickers)
-tickers.to_csv('./stock_list/stock_etf_list_'+date.today().isoformat()+'.scv')
-
-=======
 tickers = pd.DataFrame(stock.get_etf_ticker_list(to_day))
 tickers.to_csv('./stock_list/ETF_ticker_list_'+date.today().isoformat()+'.scv')
 tickers = pd.DataFrame(stock.get_market_ticker_list(to_day, market='KOSPI'))
@@ -31,7 +21,6 @@ tickers ={}
 tickers['kospi_tickers'] = ''
 tickers['kosdaq_tickers'] =''
 tickers['etf_tickers'] = ''
->>>>>>> 3d49ecd2351e6949ff54a73f183f81cfc84a1c02
 
 
 
