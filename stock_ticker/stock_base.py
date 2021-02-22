590cb4fa49eb4cdf70292fe3ba43fc93f428e2f6
seokjin window PC <hopesukjin@gmail.com>
2021-02-23 00:23:05 +0900

    TODO list 추가 2021-02-23 기존에 정보가 있을때 append 하는 처리 만들기.

--- stock_ticker/stock_base.py
+++ stock_ticker/stock_base.py
@@ -3,11 +3,10 @@ import numpy as np
 import time
 import os.path
 
+from datetime import datetime
 from pykrx import stock
 from datetime import date
 from config.log_class import Logging
-
-
 
 
 class Get_stock_ticker:
@@ -25,7 +24,6 @@ class Get_stock_ticker:
         # 1.2 개별 종목별 주식 히스토리 불러오기
         self.get_stock_data()
 
-
     def get_all_ticker_list(self):
         tickers = pd.DataFrame(stock.get_etf_ticker_list(self.to_day))
         tickers.to_csv('./stock_data_ticker/ETF_ticker_list.scv')
@@ -41,17 +39,17 @@ class Get_stock_ticker:
 
     def get_stock_data(self):
         # 코스피
-        kospi_list_data = pd.read_csv('./stock_data_ticker/KOSPI_ticker_list.scv', index_col= 0)
+        kospi_list_data = pd.read_csv('./stock_data_ticker/KOSPI_ticker_list.scv', index_col=0)
         self.logging.logger.debug("KOSPI 종목별 히스토리 수집 시작")
         for i in np.array(kospi_list_data['0'].tolist()):
             if i is not None:
-                i = str(i)
-                if os.path.isfile('./stock_data_ticker/KOSPI/KOSPI_ticker_list_'+i+'.scv'):
-                    csv_data = pd.read_csv('./stock_data_ticker/KOSPI/KOSPI_ticker_list_'+i+'.scv')
+                i = str(i).zfill(6)
+                if os.path.isfile('./stock_data_ticker/KOSPI/KOSPI_ticker_list_' + i + '.scv'):
+                    csv_data = pd.read_csv('./stock_data_ticker/KOSPI/KOSPI_ticker_list_' + i + '.scv')
                     # if sum(csv_data['날짜'] == date.isoformat()) == 1:
                 else:
-                    df = stock.get_market_ohlcv_by_date("20100101", str(self.to_day), str(i))
-                    df.to_csv('./stock_data_ticker/KOSPI/KOSPI_ticker_list_'+i+'.scv')
+                    df = stock.get_market_ohlcv_by_date("20100101", str(self.to_day), i)
+                    df.to_csv('./stock_data_ticker/KOSPI/KOSPI_ticker_list_' + i + '.scv')
                     # self.logging.logger.debug("Get KOSPI_"+str(i)+"_ticker")
                     time.sleep(1)
         self.logging.logger.debug("KOSPI 종목별 히스토리 수집 종료")
@@ -61,12 +59,12 @@ class Get_stock_ticker:
         stock_list_data = pd.read_csv('./stock_data_ticker/KOSDAQ_ticker_list.scv', index_col=0)
         for i in np.array(stock_list_data['0'].tolist()):
             if i is not None:
-                i = str(i)
-                if os.path.isfile('./stock_data_ticker/KOSDAQ/KOSDAQ_ticker_list_'+i+'.scv'):
-                    csv_data = pd.read_csv('./stock_data_ticker/KOSDAQ/KOSDAQ_ticker_list_'+i+'.scv')
+                i = str(i).zfill(6)
+                if os.path.isfile('./stock_data_ticker/KOSDAQ/KOSDAQ_ticker_list_' + i + '.scv'):
+                    csv_data = pd.read_csv('./stock_data_ticker/KOSDAQ/KOSDAQ_ticker_list_' + i + '.scv')
                     # if sum(csv_data['날짜'] == date.isoformat()) == 1:
                 else:
-                    df = stock.get_market_ohlcv_by_date("20100101", str(self.to_day), str(i))
+                    df = stock.get_market_ohlcv_by_date("20100101", str(self.to_day), i)
                     df.to_csv('./stock_data_ticker/KOSDAQ/KOSDAQ_ticker_list_' + i + '.scv')
                     # self.logging.logger.debug("Get KOSDAQ_" + str(i) + "_ticker")
                     time.sleep(1)
@@ -77,13 +75,16 @@ class Get_stock_ticker:
         stock_list_data = pd.read_csv('./stock_data_ticker/ETF_ticker_list.scv', index_col=0)
         for i in np.array(stock_list_data['0'].tolist()):
             if i is not None:
-                i = str(i)
-                if os.path.isfile('./stock_data_ticker/ETF/ETF_ticker_list_'+i+'.scv'):
-                    csv_data = pd.read_csv('./stock_data_ticker/ETF/ETF_ticker_list_'+i+'.scv')
-                    # if sum(csv_data['날짜'] == date.isoformat()) == 1:
+                i = str(i).zfill(6)
+                # TODO : 2021-02-23 기존에 정보가 있을때 append 하는 처리 만들기.
+                if os.path.isfile('./stock_data_ticker/ETF/ETF_ticker_list_' + i + '.scv'):
+                    csv_data = pd.read_csv('./stock_data_ticker/ETF/ETF_ticker_list_' + i + '.scv')
+                    if datetime.strptime(csv_data.iloc[-1]['날짜'], "%Y-%m-%d") < datetime.strptime(str(self.to_day),
+                                                                                                  "%Y-%m-%d"):
+                        df = stock.get_market_ohlcv_by_date((csv_data.iloc[-1]['날짜']).split('-'), str(self.to_day), i)
                 else:
-                    df = stock.get_market_ohlcv_by_date("20100101", str(self.to_day), str(i))
+                    df = stock.get_market_ohlcv_by_date("20100101", str(self.to_day), i)
                     df.to_csv('./stock_data_ticker/ETF/ETF_ticker_list_' + i + '.scv')
-                    # self.logging.logger.debug("Get ETF_" + str(i) + "_ticker")
+                    # self.logging.logger.debug("Get ETF_" + i + "_ticker")
                     time.sleep(1)
         self.logging.logger.debug("ETF 종목별 히스토리 수집 종료")
